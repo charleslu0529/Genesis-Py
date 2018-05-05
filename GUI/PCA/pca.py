e85667939ac0a8ede7b7a63d47cc0518c325c7be
charleslu0529 <wierdboycharley@gmail.com>
2018-05-05 22:59:45 +0200

    PCA plotting with colour difference using y-axis values

--- GUI/PCA/pca.py
+++ GUI/PCA/pca.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import wx
 import numpy as np
+import random
 from collections import defaultdict
 
 pca_evec_entries = defaultdict(list)
@@ -24,15 +25,32 @@ filename = wxFileChoice.GetPath()
 
 phe_file = open(filename, "r")
 
+eigenValues = []
+
 for line in pca_file:
     datas = line.split()
-    for idx, data in enumerate(datas):
-        pca_evec_entries[idx].append(data)
+    print("Datas = ",datas)
+    if "#eigvals" in datas[0]:
+        for data in datas:
+            eigenValues.append(data)
+    else:
+        for idx, data in enumerate(datas):
+            pca_evec_entries[idx].append(data)
+
+print("Eigenvalues: ",eigenValues,"\n")
 
 number_of_pca = len(pca_evec_entries) - 1
 
-print(pca_evec_entries)
-print(number_of_pca)
+phe_entries = defaultdict(list)
+
+
+for line in phe_file:
+    datas = line.split()
+    for idx, data in enumerate(datas):
+        phe_entries[idx].append(data)
+
+print("PCA Evec entries ",pca_evec_entries,"\n")
+print("number of pca: ",number_of_pca)
 
 # wxPcaChoiceFrame = wx.Frame(None, -1, "PCA Choice.py")
 # wxPcaChoiceFrame.SetSize(0,0,50,25)
@@ -41,15 +59,37 @@ choices = []
 for x in range(1,number_of_pca):
     choices.append(x)
 
+choice_1 = choices[0]
+choice_2 = choices[1]
 # wxPcaChoice = wx.SingleChoiceDialog(None, "Choose data set 1", "pca.py", choices)
 # wxPcaChoice.ShowModal()
 
 # dataSet_1 = wxPcaChoice.GetSelection()
 # print(dataSet_1)
+
+print("pca_evec_entries[0] = ", pca_evec_entries[0],"\n")
+print("phe Entries = ", phe_entries[0],"\n")
+print("pca entry with Choice 2 = ", pca_evec_entries[choice_2],"\n")
 shape = []
-for values in pca_evec_entries[1]:
-    shape.append(20)
-print(shape)
-plt.scatter(pca_evec_entries[1], pca_evec_entries[3],10, shape)
+
+# common_entries = set(pca_evec_entries[0]).)
+# print("Common Entries: ", common_entries)
+# for idx, value in range(1,number_of_pca):
+#     code = pca_evec_entries[idx].split(":")
+
+
+# for number in range(1,number_of_pca):
+#     for value in pca_evec_entries[number-1]:
+#         for type in phe_entries[0]:
+#             if type in value:
+#                 shape.append(pca_evec_entries[choice_2])
+
+print("shape = ",shape)
+
+# plt.scatter(pca_evec_entries[choice_1], pca_evec_entries[choice_2],10, shape)
+print("creating scatter plot with pca_evec_entries[choice_2] as shape")
+plt.scatter(pca_evec_entries[choice_1], pca_evec_entries[choice_2],10, pca_evec_entries[choice_2])
+
+print(pca_evec_entries[0])
 plt.show()
 pca_file.close()
